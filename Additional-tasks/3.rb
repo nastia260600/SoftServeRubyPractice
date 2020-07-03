ce9d120242aaa33a018f87046f1c44ea6994ffb4
nastia260600 <2606nastia00@gmail.com>
2020-07-03 12:14:59 +0300

    added other tasks

---
+++ Additional-tasks/3.rb
@@ -0,0 +1,35 @@
+#3.	Кожна планета має свій номер (порядок від Сонця). За заданим значенням номера повернути назва планети.
+class Task3
+    def task3
+    puts "Введіть  номер планети для отримання її назви: "
+    number = gets.chomp
+    number = number.to_i
+
+    mercury = "Меркурій"
+    venus = "Венера"
+    jupiter = "Юпітер"
+    earth = "Земля"
+    mars = "Марс"
+    neptune = "Нептун"
+    saturn = "Сатурн"
+    uranus = "Уран"
+    pluto = "Плутон"
+
+    while number.to_i >9 || number.to_i <1 do
+        puts "Error | спробуйте ще"
+           number = gets.chomp
+           number=number.to_i
+    end
+        case number
+        when 1 then puts"Планета #{mercury} "
+        when 2 then puts"Планета #{venus} "
+        when 3 then puts"Планета #{earth} "
+        when 4 then puts"Планета #{mars} "
+        when 5 then puts"Планета #{jupiter} "
+        when 6 then puts"Планета #{saturn} "
+        when 7 then puts"Планета #{uranus} "
+        when 8 then puts"Планета #{neptune} "
+        when 9 then puts"Планета #{pluto} "
+        end
+    end
+end
---
+++ Additional-tasks/5.rb
@@ -0,0 +1,38 @@
+=begin
+    Задано три числа. Повернути найбільше число, яке можна отримати в результаті застосування до даних чисел операцій +, *, ().
+    Числа задані в межах від 1 до 10. Числа міняти місцями не можна. Можна використовувати не всі операції (можна їх повторювати).
+=end
+class Task5
+    def task5
+    puts "Введіть три числа через пробіл в діапазоні від 1 до 10"
+    a = gets.chomp
+    a = a.split (" ")
+
+    max=0
+
+    while (a[0].to_i < 1 || a[0].to_i > 10) || (a[1].to_i < 1 || a[1].to_i > 10) || (a[2].to_i < 1 || a[2].to_i > 10) do
+            puts "Error | Спробуйте ще раз"
+            a = gets.chomp
+            a = a.split (" ")
+    end
+
+    if a[0].to_i > a[1].to_i then
+        if a[0].to_i > a[2].to_i then
+            max=a[0].to_i*(a[1].to_i+a[2].to_i)
+        elsif a[0].to_i < [2].to_i && max==0 then
+            max=(a[0].to_i+a[1].to_i)*a[2].to_i
+        end
+        if a[1].to_i > a[2].to_i && max==0 then
+            max=(a[0].to_i+a[1].to_i)*a[2].to_i
+        end
+    elsif a[0].to_i < a[1].to_i then
+        if a[0].to_i < a[2].to_i && max==0 then
+            max=(a[0].to_i+a[1].to_i)*a[2].to_i
+        elsif a[0].to_i > a[2].to_i && max==0 then
+            max=a[0].to_i*(a[1].to_i+a[2].to_i)
+        end
+    end
+
+    puts max.to_i
+    end
+end
